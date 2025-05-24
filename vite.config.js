03e33e07520ee81e1964e9ae59465248c61500b1
etherhunt2 <etherhunt2@gmail.com>
2025-05-24 17:22:04 +0530

    Configure routing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  }
 })
